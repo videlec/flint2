a8674839e572425890b9bf2f46dd86dc700a5255
William Hart <goodwillhart@googlemail.com>
2014-06-22 19:39:50 +0100

    Added cubic version of strong probable prime test.

---
+++ ulong_extras/is_strong_probabprime_cubic2_preinv.c
@@ -0,0 +1,80 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009 William Hart
+    Copyright (C) 2011 Sebastian Pancratz
+    Copyright (C) 2014 Dana Jacobsen
+    Copyright (C) 2014 William Hart
+
+******************************************************************************/
+
+#include <gmp.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int n_is_strong_probabprime_cubic2_preinv(mp_limb_t n, mp_limb_t ninv, mp_limb_t a) 
+{
+    slong i, j;
+    mp_limb_t p0, p1, t;
+    mp_limb_t pow, pow2;
+
+    t = n - 1;
+    
+    i = 0;
+    do {
+       umul_ppmm(p1, p0, t, UWORD(6148914691236517205));
+
+       if (p0 + p1 + 1 == 0) /* is divisible by 3 */
+       {
+          i++;
+          t = p1 + 1;
+       } else 
+          break;
+    } while (1);
+
+    pow = n_powmod2_preinv(a, t, n, ninv);
+
+    if (pow == 1)
+       return 1;
+
+    for (j = 0; j < i; j++)
+    {
+       pow2 = n_mulmod2_preinv(pow, pow, n, ninv); /* raise to power 3 */
+       pow2 = n_mulmod2_preinv(pow2, pow, n, ninv);
+
+       if (pow2 == 1)
+          break;
+       else
+          pow = pow2;
+    }
+
+    if (j == i)
+       return 0;
+
+    pow2 = n_mulmod2_preinv(pow, pow, n, ninv);
+    pow = n_addmod(pow2, pow, n);
+
+    if (pow == n - 1)
+       return 1;
+
+    return 0;
+}
+
---
+++ ulong_extras/test/t-is_strong_probabprime_cubic2_preinv.c
@@ -0,0 +1,64 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009, 2014 William Hart
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   long result;
+   mp_limb_t n, ninv, t;
+   FLINT_TEST_INIT(state);
+   
+   flint_printf("is_strong_probabprime_cubic2_preinv....");
+   fflush(stdout);
+   
+   for (n = 3UL; n < 1000000000000L; n+=2) /* Test that primes pass the test */
+   {
+      ninv = n_preinvert_limb(n);
+      
+      t = n - 1;
+      while ((t & 1) == 0)
+         t >>= 1;
+
+      if (!n_is_prime(n) && (n % 3) == 0 
+         && n_is_strong_probabprime2_preinv(n, ninv, 2, t)
+         && n_is_strong_probabprime_cubic2_preinv(n, ninv, 3))
+      {
+         printf("%lu is declared prime\n", n);
+      }
+
+      if ((n & 16777215) == 0 || ((n + 1) & 16777215) == 0)
+         printf("n = %ld\n", n);
+   }
+
+   FLINT_TEST_CLEANUP(state);
+   
+   flint_printf("PASS\n");
+   return 0;
+}
